cff35b149244013192192007ee89fadb3545ef10
vm <victor.mustar@gmail.com>
2020-10-25 11:02:47 +0100

    tailwind config

--- node/tailwind.config.js
+++ node/tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
   future: {
+    // defaultLineHeights: true,
+    standardFontWeights: true,
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./views/**/*.hbs"],
   theme: {
+    container: {
+      padding: {
+        default: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
+  experimental: "all",
 };
